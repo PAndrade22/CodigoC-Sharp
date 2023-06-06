4e62ba128366d92a91dcf7d24645fbda8f1e268f
PAndrade22 <131625628+PAndrade22@users.noreply.github.com>
2023-06-06 08:48:20 -0300

    Tabuada.cs

---
+++ Tabuada.cs
@@ -0,0 +1,44 @@
+using System;
+namespace Modularização
+{
+    public class tabuada
+    {
+        public static void TabuadaZero(double numero){
+        
+        double tabuada;   
+    Console.WriteLine("\n");
+    Console.WriteLine(" ------ Tabuada de Multiplicação {0} ------ \n", numero);
+    for(int i = 0; i <= 10; i++){
+      tabuada = i * numero;
+      Console.WriteLine("{0} x {1} : {2}",numero,i,tabuada);
+      }
+       Console.WriteLine("\n");
+       Console.WriteLine(" ------ Tabuada de Adição {0} ------ \n", numero);
+       for(int i = 0; i <= 10; i++){
+          tabuada = i + numero;
+          Console.WriteLine("{0} + {1} : {2}",numero,i,tabuada);
+         }
+         Console.WriteLine("\n");
+         Console.WriteLine(" ------ Tabuada de Subtração {0} ------ \n", numero);
+         for(int i = 0; i <= 10; i++){
+           tabuada = i - numero;
+           Console.WriteLine("{0} - {1} : {2}",numero,i,tabuada);
+          }
+           Console.WriteLine("\n");
+           Console.WriteLine(" ------ Tabuada de Divisão {0} ------ \n", numero);
+           for(int i = 1; i <= 10; i++){
+              tabuada = i / numero;
+              Console.WriteLine("{0} / {1} : {2}",numero,i,tabuada);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            double numero;
+            Console.WriteLine("Digite um número: ");
+            numero = double.Parse(Console.ReadLine());
+            TabuadaZero(numero);
+
+
+        }
+    }
+}
