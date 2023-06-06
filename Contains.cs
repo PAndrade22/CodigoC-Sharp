bf4fe2fec9026a534d1105bfc2cead1fdfbe9a23
PAndrade22 <131625628+PAndrade22@users.noreply.github.com>
2023-06-06 08:58:52 -0300

    Contains.cs

---
+++ Contains.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Vetor
+{ 
+  public class matriz
+  {  
+    static void Main()
+    {
+       string nome = "Pablo Silva ";
+       string sobrenome = "Silva";
+
+       Console.WriteLine(nome.Contains(sobrenome)); //verificando se o valor de sobrenome contém na variável nome
+
+       
+
+    }
+  }
+}
