ff255db826ba13109466282777f468f4b47d4396
PSILVA <131625628+PAndrade22@users.noreply.github.com>
2023-08-20 10:55:02 -0300

    Update Tabuada.cs

--- Tabuada.cs
+++ Tabuada.cs
@@ -6,24 +6,28 @@ namespace Modularização
         public static void TabuadaZero(double numero){
         
         double tabuada;   
+    //Tabuada de Multiplicação
     Console.WriteLine("\n");
     Console.WriteLine(" ------ Tabuada de Multiplicação {0} ------ \n", numero);
     for(int i = 0; i <= 10; i++){
       tabuada = i * numero;
       Console.WriteLine("{0} x {1} : {2}",numero,i,tabuada);
       }
+      //Tabuada de Adição
        Console.WriteLine("\n");
        Console.WriteLine(" ------ Tabuada de Adição {0} ------ \n", numero);
        for(int i = 0; i <= 10; i++){
           tabuada = i + numero;
           Console.WriteLine("{0} + {1} : {2}",numero,i,tabuada);
          }
+         //Tabuada de Subtração 
          Console.WriteLine("\n");
          Console.WriteLine(" ------ Tabuada de Subtração {0} ------ \n", numero);
          for(int i = 0; i <= 10; i++){
            tabuada = i - numero;
            Console.WriteLine("{0} - {1} : {2}",numero,i,tabuada);
           }
+          //Tabuada de Divisão
            Console.WriteLine("\n");
            Console.WriteLine(" ------ Tabuada de Divisão {0} ------ \n", numero);
            for(int i = 1; i <= 10; i++){
